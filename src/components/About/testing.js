630c8d9f595f6f5e98dbe5d69e7021fcbc39285f
ZJW-92 <zjwei0201@gmail.com>
2023-01-19 13:57:05 +0100

    add testing

---
+++ src/components/About/testing.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiRobotframework, SiJenkins, SiSelenium, SiGitlab, SiPostman} from "react-icons/si";
+
+function testing() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={1} className="tech-icons">
+        <SiRobotframework />
+      </Col>
+      <Col xs={2} md={1} className="tech-icons">
+        <SiJenkins />
+      </Col>
+      <Col xs={2} md={1} className="tech-icons">
+        <SiSelenium />
+      </Col>
+      <Col xs={2} md={1} className="tech-icons">
+        <SiGitlab />
+      </Col>
+      <Col xs={2} md={1} className="tech-icons">
+        <SiPostman/>
+      </Col>
+    </Row>
+  );
+}
+
+export default testing;
