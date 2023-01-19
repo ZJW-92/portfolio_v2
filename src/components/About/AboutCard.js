3dd59c54b2a95fc0878820959282278b865fa236
ZJW-92 <zjwei0201@gmail.com>
2023-01-19 13:54:25 +0100

    add aboutcard

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <p className="home-about-body">
+          <p style={{ textAlign: "justify" }}>
+          ⭑  I have genuine passion for&nbsp;
+          <strong className="main-name">Software Engineering and Development. </strong>
+          Also, I am interested with Quality Assurance, Test Automation, DevOps and Machine Learning.
+          <br /><br />      
+          ⭑ I am careful, resistant to stress, self-motivated with a&nbsp;
+          <strong className="main-name">strong </strong>
+          desire to learn new things and like to take my own initiative.<br/><br/>
+          ⭑ I am active, like to cooperate in&nbsp;
+          <strong className="main-name">teamwork </strong>
+          and open to new ideas and willing to listen to advice. <br/><br/>
+          ⭑ My biggest advantages are my competence and&nbsp;
+         <strong className="main-name">positive </strong>
+        attitude.<br/><br/>
+         </p>
+        </p>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
