2129a166d53da0f9739f80a9b28e72c7bed1efbe
ZJW-92 <zjwei0201@gmail.com>
2023-01-19 13:55:00 +0100

    add backend

---
+++ src/components/About/backend.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {SiExpress,SiGraphql} from "react-icons/si";
+
+import {DiNodejs,DiMongodb, DiNpm,DiDjango, } from "react-icons/di"
+ 
+
+
+function backend() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={2} md={1} className="tech-icons">
+        <SiExpress />
+      </Col>
+      <Col xs={2} md={1} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={2} md={1} className="tech-icons">
+        <DiNpm />
+      </Col>
+      <Col xs={2} md={1} className="tech-icons">
+        <DiDjango/>
+      </Col>
+      <Col xs={2} md={1} className="tech-icons">
+        <DiMongodb/>
+      </Col>
+      <Col xs={2} md={1} className="tech-icons">
+        <SiGraphql/>
+      </Col>
+    </Row>
+  );
+}
+
+export default backend;
