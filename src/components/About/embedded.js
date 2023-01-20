459c952d2db57880b6f95cde98120ae00438b6a1
ZJW-92 <zjwei0201@gmail.com>
2023-01-20 14:51:36 +0100

    update about

--- src/components/About/embedded.js
+++ src/components/About/embedded.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { SiCplusplus,SiPython, SiC,  SiPycharm, SiUbuntu  } from "react-icons/si";
 import {ImEmbed2} from 'react-icons/im'
-
+import {SiVmware, SiLinux} from 'react-icons/si';
 
 
 function embedded() {
@@ -19,6 +19,12 @@ function embedded() {
       </Col>
       <Col xs={2} md={1} className="tech-icons">
         < SiPycharm/>
+      </Col>
+      <Col xs={2} md={1} className="tech-icons">
+        <SiVmware/>
+      </Col>
+      <Col xs={2} md={1} className="tech-icons">
+        < SiLinux/>
       </Col>
       <Col xs={2} md={1} className="tech-icons">
         <SiUbuntu/>
--- src/components/About/others.js
+++ src/components/About/others.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {FaGit} from "react-icons/fa";
-import {SiVmware, SiLinux,SiVisualstudiocode} from 'react-icons/si';
+import {SiVisualstudiocode} from 'react-icons/si';
 import {BsFillTerminalFill} from 'react-icons/bs'
 
 function others() {
@@ -12,12 +12,6 @@ function others() {
       </Col>
       <Col xs={2} md={1} className="tech-icons">
         <BsFillTerminalFill/>
-      </Col>
-      <Col xs={2} md={1} className="tech-icons">
-        <SiVmware/>
-      </Col>
-      <Col xs={2} md={1} className="tech-icons">
-        < SiLinux/>
       </Col>
       <Col xs={2} md={1} className="tech-icons">
         < SiVisualstudiocode/>
