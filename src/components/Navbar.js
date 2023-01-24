5f6b61a98790cbd4f184fe9e97673d41fc4928a2
ZJW-92 <zjwei0201@gmail.com>
2023-01-24 14:22:37 +0100

    update navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,7 +43,7 @@ function NavBar() {
             updateExpanded(expand ? false : "expanded");
           }}
         >
-      
+        
           <span></span>
           <span></span>
           <span></span>
