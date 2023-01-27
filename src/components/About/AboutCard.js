bf4d66771a75347ca9433188395010d1c38b9898
ZJW-92 <zjwei0201@gmail.com>
2023-01-27 14:49:41 +0100

    update about card

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,12 +9,12 @@ function AboutCard() {
         <p className="home-about-body">
           <p style={{ textAlign: "justify" }}>
           ⭑  I have genuine passion for&nbsp;
-          <strong className="main-name">User Requirements and Software Engineering. </strong>
-          Also, I am interested with Quality Assurance, Test Automation, DevOps and Machine Learning.
+          <strong className="main-name">Software Engineering and User Requirements. </strong>
+          Also, I am interested with Consulting, Digitalization, Cyber Security, Quality Assurance, Test Automation, DevOps and Machine Learning.
           <br /><br />      
           ⭑ I am careful, resistant to stress, self-motivated with a&nbsp;
           <strong className="main-name">strong </strong>
-          desire to learn new things and like to take my own initiative.<br/><br/>
+          desire to keep learning and like to communicate and share ideas.<br/><br/>
           ⭑ I am active, like to cooperate in&nbsp;
           <strong className="main-name">teamwork </strong>
           and open to new ideas and willing to listen to advice. <br/><br/>
